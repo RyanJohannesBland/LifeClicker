1f759f1383d132fb9f33bac8f73c43f3d5439147
RyanJohannesBland <rjjohannesbland@email.arizona.edu>
2021-12-22 14:31:49 -0700

    Initial Commit

---
+++ LifeClicker/LifeClickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifeClickerApp.swift
+//  LifeClicker
+//
+//  Created by ryan on 12/22/21.
+//
+
+import SwiftUI
+
+@main
+struct LifeClickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
